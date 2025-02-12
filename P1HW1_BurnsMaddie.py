f6f78dc82256229d4c1af12d61232d155e6b3b1a
Burnsm473 <burnsm4731@student.faytechcc.edu>
2025-02-11 20:13:15 -0500

    Add files via upload
    P1HW1_BurnsMaddie

---
+++ P1HW1_BurnsMaddie.py
@@ -0,0 +1,22 @@
+# Maddie Burns
+# (1/11/25)
+# P1HW1
+# Use Python's input and print functions to create a program that shows output.
+
+#Calculating Exponents
+
+x = float(input("Enter an integer as the base value:"))
+y = float(input("Enter an integer as the exponent:"))
+z = x ** y
+
+print(f"{x} raised to the power of {y} is {z}")
+
+# Addition and subtraction
+
+a = float(input("Enter a starting integer:"))
+b = float(input("Enter an integer to add:"))
+c = float(input("Enter an integer to subtract:"))
+d = a + b - c
+
+print(f"{a} + {b} - {c} is equal to {d}")
+
