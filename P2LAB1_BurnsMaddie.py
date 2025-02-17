6fa959a934fb831719ec50df55597a0f6cc30a3c
Burnsm473 <burnsm4731@student.faytechcc.edu>
2025-02-17 14:49:30 -0500

    Add files via upload
    P2LAB1_BurnsMaddie

---
+++ P2LAB1_BurnsMaddie.py
@@ -0,0 +1,17 @@
+# Maddie Burns
+# (2/17/25)
+# P2Lab1
+
+
+
+
+radius = float(input("What is the radius of the circle?"))
+diameter = radius * 2
+circumference = 3.14 * diameter
+area = 3.141 * (radius**2)
+
+
+print("The diameter of the circle is:", diameter)
+print("The circumference of the circle is", circumference)
+print("The area of the circle is", area)
+
